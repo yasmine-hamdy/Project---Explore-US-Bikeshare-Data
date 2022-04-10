5eed5810d97a590ba337049f68c639827a7bfbda
Yasmine Hamdy <yasminehamdy@aucegypt.edu>
2022-04-10 15:39:56 +0200

    load data and return dataframe

--- proj_code.py
+++ proj_code.py
@@ -38,3 +38,39 @@ def get_filters():
 
     print('-'*40)
     return city, month, day
+
+
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+
+    # load data file into a DataFrame
+    df = pd.read_csv(CITY_DATA[city])
+
+    # convert the Start Time column to datetime
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+
+    # extract month and day of week from Start Time to create new columns
+    df['month'] = df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.day_name()
+
+    # filter by month if applicable
+    if month != 'all':
+        month = months.index(month) + 1
+        # filter by month to create the new DataFrame
+        df = df[df['month'] == month]
+
+    # filter by day of week if applicable
+    if day != 'all':
+        # filter by day of week to create the new DataFrame
+        df = df[df['day_of_week'] == day.title()]
+
+    return df
