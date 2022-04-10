92ff164ec6a3b50ce71456a36902ad1cc0865fff
Yasmine Hamdy <yasminehamdy@aucegypt.edu>
2022-04-10 15:35:08 +0200

    create city dictionary

--- proj_code.py
+++ proj_code.py
@@ -1,3 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
+
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
