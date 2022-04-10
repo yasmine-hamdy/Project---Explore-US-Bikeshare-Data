3f4ca83f9d98e6c92cba869b7df31e1206a485f0
Yasmine Hamdy <yasminehamdy@aucegypt.edu>
2022-04-10 15:36:01 +0200

    create months list

--- proj_code.py
+++ proj_code.py
@@ -5,3 +5,6 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
+              
+
+months = ['january', 'february', 'march', 'april', 'may', 'june', 'all']
